bc8d16b5e01bc374bcb75de9a5aa586ac903a3b0
souenxx <souen0823@gmail.com>
2024-05-22 09:25:48 +0900

    style: teams-component

---
+++ front/app/src/components/pages/categories/teams/style.tsx
@@ -0,0 +1,31 @@
+"use client";
+import styled from "styled-components";
+
+export const StyledWrapperDiv = styled.div``;
+
+export const StyledTeamsList = styled.ul`
+  padding: 0;
+`;
+
+export const StyledTeamsListItem = styled.li`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  margin: 30px 0;
+`;
+
+export const StyledContentSubTitleDiv = styled.div``;
+
+export const StyledContentSubTitleP = styled.p`
+  font-size: 14px;
+  color: #a69e9e;
+  text-align: center;
+`;
+
+export const StyledContentTitleDiv = styled.div``;
+
+export const StyledContentTitleP = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+`;
